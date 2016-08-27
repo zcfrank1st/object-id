549625e96612b7f0aef38ea5a2c23b244b5838ed
zcfrank1st <zcfrank1st@sina.com>
2016-08-27 10:43:35 +0800

    add test main

---
+++ src/test/java/Main.java
@@ -0,0 +1,16 @@
+import com.chaos.object.id.core.ObjectId;
+
+/**
+ * Created by zcfrank1st on 8/27/16.
+ */
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ObjectId objectId = new ObjectId();
+        while (true) {
+            Thread.sleep(1000);
+            long id = objectId.getObjectId();
+            System.out.println(id);
+            System.out.println(Long.toHexString(id));
+        }
+    }
+}

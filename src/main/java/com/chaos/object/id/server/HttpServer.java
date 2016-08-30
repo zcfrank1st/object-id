f7b6495d3c7d16f6e722b9c56554dfa4897d4c26
zcfrank1st <zcfrank1st@sina.com>
2016-08-30 09:28:48 +0800

    use nanohttpd to replace embedded tomcat

--- src/main/java/com/chaos/object/id/server/HttpServer.java
+++ src/main/java/com/chaos/object/id/server/HttpServer.java
@@ -1,48 +1,34 @@
 package com.chaos.object.id.server;
 
+import com.chaos.object.id.core.ObjectId;
 import com.chaos.object.id.util.ConfigLoader;
-import com.google.inject.Inject;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.WebResourceRoot;
-import org.apache.catalina.core.StandardContext;
-import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.DirResourceSet;
-import org.apache.catalina.webresources.StandardRoot;
+import fi.iki.elonen.NanoHTTPD;
 
-import javax.servlet.ServletException;
-import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
 
 /**
  * Created by zcfrank1st on 8/27/16.
  */
-public class HttpServer {
-    final private static String port = new ConfigLoader().getConf().getString("http.server.port");
+public class HttpServer extends NanoHTTPD {
+    final private static int port = new ConfigLoader().getConf().getInt("http.server.port");
 
-    public static void main(String[] args) throws ServletException, LifecycleException {
-        String webAppDirLocation = "src/main/resources/";
-        Tomcat tomcat = new Tomcat();
+    public HttpServer(int port) throws IOException {
+        super(port);
+        start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+        System.out.println("\nRunning! http://" + InetAddress.getLocalHost().getHostAddress() + ":8080/ \n");
+    }
 
-        //The port that we should run on can be set into an environment variable
-        //Look for that variable and default to 8080 if it isn't there.
-        String webPort = System.getenv("PORT");
-        if(webPort == null || webPort.isEmpty()) {
-            webPort = port;
+    public static void main(String[] args) {
+        try {
+            new HttpServer(port);
+        } catch (IOException ioe) {
+            System.err.println("Couldn't start server:\n" + ioe);
         }
+    }
 
-        tomcat.setPort(Integer.valueOf(webPort));
-
-        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webAppDirLocation).getAbsolutePath());
-
-        // Declare an alternative location for your "WEB-INF/classes" dir
-        // Servlet 3.0 annotation will work
-        File additionWebInfClasses = new File("target/classes");
-        WebResourceRoot resources = new StandardRoot(ctx);
-
-        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
-                additionWebInfClasses.getAbsolutePath(), "/"));
-        ctx.setResources(resources);
-
-        tomcat.start();
-        tomcat.getServer().await();
+    @Override
+    public Response serve(IHTTPSession session) {
+        return newFixedLengthResponse(Long.toHexString(new ObjectId().getObjectId()));
     }
 }

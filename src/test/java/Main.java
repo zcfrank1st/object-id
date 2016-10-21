41e1cf3d653a3ba9807871536631cf0c068ca50e
zcfrank1st <zcfrank1st@sina.com>
2016-10-21 16:16:09 +0800

    remove hashids jar

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,16 +1,17 @@
 import com.chaos.object.id.core.ObjectId;
+import org.hashids.Hashids;
 
 /**
  * Created by zcfrank1st on 8/27/16.
  */
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        ObjectId objectId = new ObjectId();
-        while (true) {
-            Thread.sleep(1000);
-            long id = objectId.getObjectId();
-            System.out.println(id);
-            System.out.println(Long.toHexString(id));
-        }
+//        ObjectId objectId = new ObjectId();
+//        while (true) {
+//            Thread.sleep(1000);
+//            long id = objectId.getObjectId();
+//            System.out.println(id);
+//            System.out.println(Long.toHexString(id));
+//        }
     }
 }
